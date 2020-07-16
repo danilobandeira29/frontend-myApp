6e275f1826174e72ef49859f0a2cdc5d535159a6
Danilo Bandeira <danilobandeira29@gmail.com>
2020-07-16 02:15:43 -0300

    able to add toast messages

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -3,47 +3,28 @@ import React from 'react';
 import { FiXCircle, FiAlertCircle } from 'react-icons/fi';
 import './styles.css';
 
-interface ToastProps {
-  className?: string;
+import { ToastMessage } from '../../hooks/toast';
+
+interface ToastContainerProps {
+  messages: ToastMessage[];
 }
 
-const Toast: React.FC<ToastProps> = ({ className }) => {
+const Toast: React.FC<ToastContainerProps> = ({ messages }) => {
   return (
-    <div className={`toast-container ${className}`}>
-      <div className="toast-content">
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um Erro</strong>
-          <span>Não foi possível fazer login na aplicação.</span>
+    <div className="toast-container">
+      {messages.map(message => (
+        <div key={message.id} className={`toast-content ${message.type}`}>
+          <FiAlertCircle size={20} />
+          <div>
+            <strong>{message.title}</strong>
+            <span>{message.description}</span>
+          </div>
+
+          <button type="button" className="button-toast-container">
+            <FiXCircle size={18} />
+          </button>
         </div>
-
-        <button type="button" className="button-toast-container">
-          <FiXCircle size={18} />
-        </button>
-      </div>
-
-      <div className="toast-content sucess">
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um Erro</strong>
-          <span>Não foi possível fazer login na aplicação.</span>
-        </div>
-
-        <button type="button" className="button-toast-container">
-          <FiXCircle size={18} />
-        </button>
-      </div>
-      <div className="toast-content error">
-        <FiAlertCircle size={20} />
-        <div>
-          <strong>Aconteceu um Erro</strong>
-          <span>Não foi possível fazer login na aplicação.</span>
-        </div>
-
-        <button type="button" className="button-toast-container">
-          <FiXCircle size={18} />
-        </button>
-      </div>
+      ))}
     </div>
   );
 };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -49,7 +49,11 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na Autenticação',
+          description: 'Ocorreu um erro ao fazer login, cheque as credencias.',
+        });
       }
     },
     [signIn, addToast],
